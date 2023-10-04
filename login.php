00c64c37c6eaaa5cb7cd319e3be880a5788a50ab
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-05 00:50:37 +0700

    Update login.php

--- login.php
+++ login.php
@@ -11,9 +11,10 @@ $database = "utt";
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
-}
+} 
+// else {
+//     echo "Connected successfully";
+// }
     $User = $_POST["User"];
     $Password = $_POST["Password"];
 
