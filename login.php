33208a11ac326cb7352a72bd8755420e28aa1be9
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-04 08:56:31 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,10 +3,18 @@
     $User = $_POST["User"];
     $Password = $_POST["Password"];
 
+
+ $host = $_ENV['DB_HOST'];
+    $database = 'utt'; // Replace with your database name
+    $username = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASSWORD'];
+
+
+
     //tao ket noi
-    $conn = mysqli_connect("localhost","root","") or die ("Không connect đc với máy chủ");
+    $conn = mysqli_connect($host, $username, $password) or die ("Không connect đc với máy chủ");
     //tim csdl de lam viec
-    mysqli_select_db($conn,"utt") or die;
+    mysqli_select_db($conn,$database) or die;
     // cau lenh query
     $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
 
