07b1599a63c50593e9735a6d1e09fefd623ffc92
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-05 00:43:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,4 @@
 <?php
-    // lay dulieu tu form dangnhap
-    $User = $_POST["User"];
-    $Password = $_POST["Password"];
-
 $host = "yepps.mysql.database.azure.com";
 $username = "baoanhhihi";
 $password = "Vuchien@123";
@@ -18,16 +14,28 @@ if ($conn->connect_error) {
 } else {
     echo "Connected successfully";
 }
-    // cau lenh query
-    $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
+     echo $User = $_POST["User"];
+    echo $Password = $_POST["Password"];
 
+    //trich xuat du lieu nhap bang ham 
+    $Input_user = mysqli_real_escape_string($conn, $User);
+    $Input_pass = mysqli_real_escape_string($conn, $Password);
 
-    //thuc hien truy van
-    $result = mysqli_query($conn, $sql_insert_account);
+   echo $sql = "SELECT * FROM `account` WHERE User = '$Input_user' and Password = '$Input_pass'";
+   
+
+    $result = mysqli_query($conn, $sql);
+
+    // tắt hiển thị lỗi
+    // ini_set('display_errors', '0');
+
     $count = mysqli_num_rows($result);
+    $row = mysqli_fetch_assoc($result); //hien thi len form
 
     if($count == 1){
+        session_start();
         $_SESSION['User_name'] = $User;
+        $_SESSION['id'] = 1; 
         header('Location: index.php');
     } else{
         header('Location: login.html');
