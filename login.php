a48ff934e8305c8cc305583435ee65b8cbf25369
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-05 14:55:28 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,22 @@
 <?php
-    // lay dulieu tu form dangnhap
-    $User = $_POST["User"];
-    $Password = $_POST["Password"];
-
-    //tao ket noi
+  //tao ket noi
     $host = "yepps.mysql.database.azure.com";
     $username = "baoanhhihi";
     $password = "Vuchien@123";
     $database = "utt";
-    
-    
-        //tao ket noi
-        $conn = new mysqli($host, $username, $password, $database);
+
+
+    //tao ket noi
+    $conn = new mysqli($host, $username, $password, $database);
     
     // Check the connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
 
+    // lay dulieu tu form dangnhap
+    $User = $_POST["User"];
+    $Password = $_POST["Password"];
 
     //thuc hien truy van
     $result = mysqli_query($conn, $sql_insert_account);
