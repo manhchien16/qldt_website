7cfbc539988c5f0d9dfb9c3538b058457c42f8fb
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-05 14:39:58 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,74 @@
  <?php
-$host = "yepps.mysql.database.azure.com";
-$username = "baoanhhihi";
-$password = "Vuchien@123";
-$database = "utt";
+// $host = "yepps.mysql.database.azure.com";
+// $username = "baoanhhihi";
+// $password = "Vuchien@123";
+// $database = "utt";
 
 
-    //tao ket noi
-    $conn = new mysqli($host, $username, $password, $database);
+//     //tao ket noi
+//     $conn = new mysqli($host, $username, $password, $database);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-// else {
-//     echo "Connected successfully";
-// }
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// // else {
+// //     echo "Connected successfully";
+// // }
+//     $User = $_POST["User"];
+//     $Password = $_POST["Password"];
+
+//     //trich xuat du lieu nhap bang ham 
+//     $Input_user = mysqli_real_escape_string($conn, $User);
+//     $Input_pass = mysqli_real_escape_string($conn, $Password);
+
+//     $sql = "SELECT * FROM `account` WHERE User = '$Input_user' and Password = '$Input_pass'";
+   
+
+//     $result = mysqli_query($conn, $sql);
+
+//     // tắt hiển thị lỗi
+//     // ini_set('display_errors', '0');
+
+//     if($count == 1){
+//         session_start();
+//         $_SESSION['User_name'] = $User;
+//         $_SESSION['id'] = 1; 
+//         header('Location: index.php');
+//     } else{
+//         header('Location: login.html');
+//     }
+?>
+
+
+<?php
+    // lay dulieu tu form dangnhap
     $User = $_POST["User"];
     $Password = $_POST["Password"];
 
-    //trich xuat du lieu nhap bang ham 
-    $Input_user = mysqli_real_escape_string($conn, $User);
-    $Input_pass = mysqli_real_escape_string($conn, $Password);
+    //tao ket noi
+    $host = "yepps.mysql.database.azure.com";
+    $username = "baoanhhihi";
+    $password = "Vuchien@123";
+    $database = "utt";
+    
+    
+        //tao ket noi
+        $conn = new mysqli($host, $username, $password, $database);
+    
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
 
-    $sql = "SELECT * FROM `account` WHERE User = '$Input_user' and Password = '$Input_pass'";
-   
 
-    $result = mysqli_query($conn, $sql);
-
-    // tắt hiển thị lỗi
-    // ini_set('display_errors', '0');
+    //thuc hien truy van
+    $result = mysqli_query($conn, $sql_insert_account);
+    $count = mysqli_num_rows($result);
 
     if($count == 1){
-        session_start();
         $_SESSION['User_name'] = $User;
-        $_SESSION['id'] = 1; 
-        header('Location: index.php');
+        header('Location: index.html');
     } else{
         header('Location: login.html');
     }
