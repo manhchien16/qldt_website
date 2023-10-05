a1e9274143bd2e08d9378e22e9f3d32f6a16c839
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-05 14:59:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -12,12 +12,13 @@
     // Check the connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
+    
 
     // lay dulieu tu form dangnhap
     $User = $_POST["User"];
     $Password = $_POST["Password"];
 
+    $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
     //thuc hien truy van
     $result = mysqli_query($conn, $sql_insert_account);
     $count = mysqli_num_rows($result);
