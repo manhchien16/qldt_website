121b0a6826a95e67a25fb4bd5eadc81d9dcc2c87
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-06 00:17:05 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,26 @@
-<?php
-  //tao ket noi
-    $host = "yepps.mysql.database.azure.com";
-    $username = "baoanhhihi";
-    $password = "Vuchien@123";
-    $database = "utt";
-
-
-    //tao ket noi
-    $conn = new mysqli($host, $username, $password, $database);
-    
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    
-
+  
+    <?php
     // lay dulieu tu form dangnhap
     $User = $_POST["User"];
     $Password = $_POST["Password"];
 
-    $sql_insert_account ="SELECT * FROM `account` WHERE User = '$User' and Password = '$Password'";
+    //tao ket noi
+    $conn = mysqli_connect("yepps.mysql.database.azure.com","baoanhhihi","Vuchien@123") or die;
+    //tim csdl de lam viec
+    mysqli_select_db($conn,"utt") or die;
+    // cau lenh query
+    $sql_insert_account = "SELECT * FROM account WHERE User = '$User' and Password = '$Password'";
+
+
     //thuc hien truy van
     $result = mysqli_query($conn, $sql_insert_account);
     $count = mysqli_num_rows($result);
 
     if($count == 1){
         $_SESSION['User_name'] = $User;
-        header('Location: index.php');
+        header('Location: index.html');
     } else{
         header('Location: login.html');
     }
 ?>
+
